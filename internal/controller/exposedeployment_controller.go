3c89be11c1bf7934cba1b4a7cd7b83b752b059ba
Sior Meir <Sior Meir>
2025-07-10 10:20:33 +0300

    fix: change names

--- internal/controller/exposedeployment_controller.go
+++ internal/controller/exposedeployment_controller.go
@@ -82,7 +82,7 @@ func (r *ExposeDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Req
 						Name:    "expose-deployment",
 						Image:   exposedeploy.Spec.Image,
 						Command: []string{"sleep", "infinity"},
-						Args:    exposedeploy.Spec.Env,
+						Args:    exposedeploy.Spec.CustomEnv,
 					}},
 				},
 			}
